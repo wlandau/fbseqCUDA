8cc14590e7d78372413fa22174142c45453316c9
wlandau <will.landau@gmail.com>
2015-10-26 12:21:31 -0500

    rename

---
+++ src/slice/args.h
@@ -0,0 +1,12 @@
+#ifndef ARGS_H
+#define ARGS_H
+
+#define MAX_STEPS 300
+#define STEP_WIDTH 5.0
+
+typedef struct {
+  int max_steps, step_width, target_type;
+  double A, B, C, D, E, G, idx, rate, scale, shape, upperbound, x, x0;
+} args_t;
+
+#endif // ARGS_H
---
+++ src/slice/slice.h
@@ -0,0 +1,34 @@
+#ifndef SLICE_H
+#define SLICE_H
+
+__device__ double slice(chain_t *dd, args_t args){
+
+  double x;
+  double lu = ltarget(dd, args, args.x0) + log(runiform(dd, args.idx, 0.0, 1.0));
+  double L = args.x0 - runiform(dd, args.idx, 0.0, args.step_width);
+  double R = L + args.step_width;
+
+  int stepsL = args.max_steps;
+  int stepsR = args.max_steps;
+
+  while ((lu < ltarget(dd, args, L)) && (stepsL > 0)) {
+    L = L - args.step_width;
+    stepsL = stepsL - 1;
+  }
+
+  while ((lu < ltarget(dd, args, R)) & (stepsR > 0)) {
+    R = R + args.step_width;
+    stepsR = stepsR - 1;
+  }
+
+  do {
+    x = runiform(dd, args.idx, L, R);
+    if (x > args.x0)
+      R = x;
+    else
+      L = x;
+  } while(ltarget(dd, args, x) < lu);
+  return x;
+}
+
+#endif // SLICE_H
