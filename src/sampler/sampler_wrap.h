bf1dd7de501d558b9f241091315e4b7c6d38f62e
wlandau <will.landau@gmail.com>
2015-12-29 19:38:57 -0500

    no more metropolis

--- src/sampler/sampler_wrap.h
+++ src/sampler/sampler_wrap.h
@@ -3,12 +3,10 @@
 
 #define DEFAULT 1
 #define SLICE_STEP 2
-#define METROPOLIS 3
 
 __device__ args_t sampler_wrap(chain_t *dd, args_t args){
   switch(args.sampler){
     case SLICE_STEP : return slice_step(dd, args);
-//    case METROPOLIS : return metropolis(dd, args); // Not tested or fully implemented.
     default         : return slice_step(dd, args);
   }
 }
