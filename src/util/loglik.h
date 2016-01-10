6b58455de8541e67b0086108792d4a9b1314f639
wlandau <will.landau@gmail.com>
2016-01-09 22:40:57 -0600

    compiles

--- src/util/loglik.h
+++ src/util/loglik.h
@@ -7,7 +7,7 @@ __global__ void loglik_kernel1(chain_t *dd){
   for(n = IDY; n < dd->N; n += NTHREADSY){
     for(g = IDX; g < dd->G; g += NTHREADSX){
       lambda = dd->h[n] + dd->epsilon[I(n, g)] + Xbeta(dd, n, g);
-      dd->aux[I(n, g)] = -lambda + dd->y[I(n, g)] * log(lambda) - lgamma(dd->y[I(n, g)] + 1);
+      dd->aux[I(n, g)] = -lambda + dd->counts[I(n, g)] * log(lambda) - lgamma((double) dd->counts[I(n, g)] + 1.0);
     }
   }
 }
