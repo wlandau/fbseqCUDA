822f647e59d499be807190235836f2869cb9ef9d
wlandau <will.landau@gmail.com>
2016-04-28 21:01:16 -0500

    More hard coding...

--- src/util/cuda_usage.h
+++ src/util/cuda_usage.h
@@ -14,11 +14,11 @@
 #define MAX(a, b) (a > b ? a : b)
 #define MIN(a, b) (a < b ? a : b)
 
-#define IDX ((blockIdx.x * blockDim.x) + threadIdx.x)
-#define IDY ((blockIdx.y * blockDim.y) + threadIdx.y)
+#define IDX 0 // ((blockIdx.x * blockDim.x) + threadIdx.x)
+#define IDY 0 // ((blockIdx.y * blockDim.y) + threadIdx.y)
 
-#define NTHREADSX (blockDim.x * gridDim.x)
-#define NTHREADSY (blockDim.y * gridDim.y)
+#define NTHREADSX 1 // (blockDim.x * gridDim.x)
+#define NTHREADSY 1 // (blockDim.y * gridDim.y)
 
 #define CUDA_CALL(x) {if((x) != cudaSuccess){ \
   REprintf("CUDA error at %s:%d\n",__FILE__,__LINE__); \
